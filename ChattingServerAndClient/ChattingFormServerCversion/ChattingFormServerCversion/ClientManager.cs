a56ec35cc3bb0c0b940afcc70a977b05349445a8
saaut <saaut60@naver.com>
2022-12-03 13:01:01 +0900

    Chatting_DB
    내역이 db에 추가되고,chattingRoom이 구분되는 chatting

--- ChattingServerAndClient/ChattingFormServerCversion/ChattingFormServerCversion/ClientManager.cs
+++ ChattingServerAndClient/ChattingFormServerCversion/ChattingFormServerCversion/ClientManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +16,16 @@ namespace ChattingFormServerCversion
         public static ConcurrentDictionary<int, ClientData> clientDic = new ConcurrentDictionary<int, ClientData>();
         public static event Action<string, string>? messageParsingAction = null;
         public static event Action<string, int>? ChatngeListBoxAction = null;
+        DBManager dbmanager = DBManager.GetInstance();
 
         public void AddClient(TcpClient newClient)
         {
             ClientData currentClient = new(newClient);
 
             try
-            {
+            {//받아온 아이디 검색해서 db에 이름 혹은 닉네임 검색하면 된다
                 newClient.GetStream().BeginRead(currentClient.readBuffer, 0, currentClient.readBuffer.Length, new AsyncCallback(DataReceived), currentClient);
-                clientDic.TryAdd(currentClient.clientNumber, currentClient);
+                clientDic.TryAdd(currentClient.clientNumber, currentClient);//유저 식별 번호와, 그외 유저 정보들 ADD
             }
 
             catch (Exception e)
@@ -42,20 +44,20 @@ namespace ChattingFormServerCversion
             {
                 int byteLength = client.tcpClient.GetStream().EndRead(ar);
 
-                string strData = Encoding.Default.GetString(client.readBuffer, 0, byteLength);
+                string strData = UTF8Encoding.UTF8.GetString(client.readBuffer, 0, byteLength);
 
                 client.tcpClient.GetStream().BeginRead(client.readBuffer, 0, client.readBuffer.Length, new AsyncCallback(DataReceived), client);
 
-                if (string.IsNullOrEmpty(client.clientName))
+                if (string.IsNullOrEmpty(client.clientID))
                 {
                     if (ChatngeListBoxAction != null)
                     {
                         if (CheckID(strData))
                         {
-                            string userName = strData.Substring(3);
-                            client.clientName = userName;
-                            ChatngeListBoxAction.Invoke(client.clientName, StaticDefine.ADD_USER_LIST);
-                            string accessLog = string.Format("[{0}] {1} Access Server", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), client.clientName);
+                            string userID = strData.Substring(3);//로그인때 사용한 ID
+                            client.clientID = userID;//ID를 통해 본인의 이름(), 닉네임 저장해두자.
+                            ChatngeListBoxAction.Invoke(client.clientID, StaticDefine.ADD_USER_LIST);
+                            string accessLog = string.Format("[{0}] {1} Access Server", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), client.clientID);
                             ChatngeListBoxAction.Invoke(accessLog, StaticDefine.ADD_ACCESS_LIST);
                             File.AppendAllText("AccessRecored.txt", accessLog + "\n");
                             return;
@@ -66,7 +68,7 @@ namespace ChattingFormServerCversion
 
                 if (messageParsingAction != null)
                 {
-                    Task task = Task.Run(() => formServer1.MessageParsing(client.clientName, strData));
+                    Task task = Task.Run(() => formServer1.MessageParsing(client.clientID, strData));
 
                 }
 
--- ChattingServerAndClient/ChattingFormServerCversion/ChattingFormServerCversion/FormServer.cs
+++ ChattingServerAndClient/ChattingFormServerCversion/ChattingFormServerCversion/FormServer.cs
@@ -16,8 +16,13 @@ namespace ChattingFormServerCversion
         private object lockObj = new object();
         public static List<string>? chattingLogList = new List<string>();
         public static List<string>? userList = new List<string>();
+        public static List<string>? usersList = new List<string>();
         public static List<string>? AccessLogList = new List<string>();
         Task? conntectCheckThread = null;//연결을 체크하는 스레드
+        string roomID = null;
+
+        DBManager dbmanager = DBManager.GetInstance();
+
         public FormServer()
         {
             
@@ -186,84 +191,54 @@ namespace ChattingFormServerCversion
             }
         }
 
-        private void SendMsgToClient(List<string> msgList, string sender)
-        {
+        private void SendMsgToClient(List<string> msgList, string senderID)//클라이언트에게 받은 메세지에 따라서 응답
+        {//sender는 id가 따로
+            //receiver는 id가 msg안에 포함
             string parsedMessage = "";
-            string receiver = "";
+            string receiverID = "";
+            string receiverNickName = "";
+            string senderNickName = "";
+            senderNickName = dbmanager.RunQuery("SELECT USER_nickname FROM s5469698.USER WHERE ID=\"" + senderID+"\"");//로그인 시 본인의 이름, ID 저장해둘 수 있음
 
-            
             int senderNumber = -1;
             int receiverNumber = -1;
 
             foreach (var item in msgList)
             {
-                string[] splitedMsg = item.Split('<');
+                string[] splitedMsg = item.Split('<');//앞부분 받는사람, 뒷부분 메세지
 
-                receiver = splitedMsg[0];
-                parsedMessage = string.Format("{0}<{1}>", sender, splitedMsg[1]);
+                receiverID = splitedMsg[0];
+                parsedMessage = string.Format("{0}%{1}<{2}>", senderID, senderNickName, splitedMsg[1]);//sendMsg, RoomList용 nickname splited안됐음ㄴ
 
-                if (parsedMessage.Contains("<GroupChattingStart>"))
+
+                    string[] splitSender = receiverID.Split("%");//받는사람 ID랑 닉네임 구분해줌
+                    receiverID = splitSender[0];
+                receiverNickName = splitSender[1];
+                
+                senderNumber = GetClinetNumber(senderID);//ID를 고유 번호로 하여 전송 구분에 사용한다.
+                receiverNumber = GetClinetNumber(receiverID);
+
+                //0.상대방이 접속 중이 아니라면 table에만 저장해둔다.
+                if (senderNumber == -1 || receiverNumber == -1)//유저 현재 접속 중이 아님?
                 {
-                    string[] groupSplit = receiver.Split('#');
 
-                    foreach (var el in groupSplit)
-                    {
-                        if (string.IsNullOrEmpty(el))
-                            continue;
-                        string groupLogMessage = string.Format(@"[{0}] [{1}] -> [{2}] , {3}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), groupSplit[0], el, splitedMsg[1]);
-                        ChangeListBox(groupLogMessage, StaticDefine.ADD_CHATTING_LIST);
-
-                        receiverNumber = GetClinetNumber(el);
-
-                        parsedMessage = string.Format("{0}<GroupChattingStart>", receiver);
-                        byte[] sendGroupByteData = Encoding.Default.GetBytes(parsedMessage);
-                        ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(sendGroupByteData, 0, sendGroupByteData.Length);
-                    }
-                    return;
-                }
-
-                if (receiver.Contains('#'))
-                {
-                    string[] groupSplit = receiver.Split('#');
-
-                    foreach (var el in groupSplit)
-                    {
-                        if (string.IsNullOrEmpty(el))
-                            continue;
-                        if (el == groupSplit[0])
-                            continue;
-                        string groupLogMessage = string.Format(@"[{0}] [{1}] -> [{2}] , {3}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), groupSplit[0], el, splitedMsg[1]);
-                        ChangeListBox(groupLogMessage, StaticDefine.ADD_CHATTING_LIST);
-
-                        receiverNumber = GetClinetNumber(el);
-
-                        parsedMessage = string.Format("{0}<{1}>", receiver, splitedMsg[1]);
-                        byte[] sendGroupByteData = Encoding.Default.GetBytes(parsedMessage);
-                        ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(sendGroupByteData, 0, sendGroupByteData.Length);
-                    }
-                    return;
-                }
-
-
-                senderNumber = GetClinetNumber(sender);
-                receiverNumber = GetClinetNumber(receiver);
-
-
-                if (senderNumber == -1 || receiverNumber == -1)
-                {
-                    //File.AppendAllText("ClientNumberErrorLog.txt", sender + receiver);
+                    dbmanager.RunQuery("insert into Chatting VALUES("+ IsRoom(senderNickName, receiverNickName)//접속 중이 아니라면 chatting table에 저장
+                        + ",\"" + receiverID + "\",\"" + senderID + "\",\"" + splitedMsg[1] + "\",\"n\",\"" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\")");
+                    MessageBox.Show("유저가 접속 중이 아님");
                     return;
                 }
 
                 byte[] sendByteData = new byte[parsedMessage.Length];
                 sendByteData = Encoding.Default.GetBytes(parsedMessage);
 
+                //1.현재 유저 리스트 보내기
                 if (parsedMessage.Contains("<GiveMeUserList>"))
                 {
                     string userListStringData = "관리자<";
                     foreach (var el in userList)
                     {
-                        userListStringData += string.Format("${0}", el);
+                        string elName = dbmanager.RunQuery("SELECT USER_nickname FROM s5469698.USER WHERE ID=\"" + el+"\"");
+                        userListStringData += string.Format("${0}%{1}", el, elName);
                     }
                     userListStringData += ">";
                     byte[] userListByteData = new byte[userListStringData.Length];
@@ -271,44 +246,104 @@ namespace ChattingFormServerCversion
                     ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(userListByteData, 0, userListByteData.Length);
                     return;
                 }
-
-
-
-
-                string logMessage = string.Format(@"[{0}] [{1}] -> [{2}] , {3}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), sender, receiver, splitedMsg[1]);
-                //ChangeListBox(logMessage, StaticDefine.ADD_CHATTING_LIST);
-
-                if (parsedMessage.Contains("<ChattingStart>"))
+                //2.존재 전체 유저 리스트 보내기->트리로 어떻게 되나?
+                if (parsedMessage.Contains("<GiveMeAllUserList>"))
                 {
-                    parsedMessage = string.Format("{0}<ChattingStart>", receiver);
-                    sendByteData = Encoding.Default.GetBytes(parsedMessage);
-                    ClientManager.clientDic[senderNumber].tcpClient.GetStream().Write(sendByteData, 0, sendByteData.Length);
+                    string userListStringData = "모든유저<";
+                    
+                    string[] AllNickName = dbmanager.RunQueryArray("SELECT USER_nickname FROM s5469698.USER ");// 닉네임 모두 불러옴 일단베열.
+                    string[] AllID = dbmanager.RunQueryArray("SELECT USER_ID FROM s5469698.USER ");// 닉네임 모두 불러옴 일단베열.
 
-                    parsedMessage = string.Format("{0}<ChattingStart>", sender);
-                    sendByteData = Encoding.Default.GetBytes(parsedMessage);
-                    ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(sendByteData, 0, sendByteData.Length);
+                    for (int i = 0; i < AllID.Length; i++)
+                    {
+                        userListStringData += string.Format("${0}%{1}", AllID[i], AllNickName[i]);//형태맞춰서
+                    }
 
+                    userListStringData += ">";
+                    byte[] userListByteData = new byte[userListStringData.Length];
+                    userListByteData = Encoding.Default.GetBytes(userListStringData);
+                    ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(userListByteData, 0, userListByteData.Length);
+                    return;
+                }
+                //3.내가 속한 방 리스트 보내기
+                if (parsedMessage.Contains("<GiveMeRoomList>"))
+                {
+                    string userListStringData = "채팅방<";
+                    List<string> RoomList = new List<string>();
+
+                    RoomList.AddRange(dbmanager.RunQueryArray("select user1 from Room where user2=\""+senderNickName+"\" GROUP BY idRoom;"));//찾는 유저가 user2에 있든 user1에 있든 찾을 수 있도록
+                    RoomList.AddRange(dbmanager.RunQueryArray("select user2 from Room where user1=\""+ senderNickName + "\" GROUP BY idRoom;"));
+
+                    foreach (var itemRoom in RoomList)
+                    {
+                        userListStringData += string.Format("${0}", itemRoom);//형태맞춰서
+                    }
+
+                    userListStringData += ">";
+                    byte[] userListByteData = new byte[userListStringData.Length];
+                    userListByteData = Encoding.Default.GetBytes(userListStringData);
+                    ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(userListByteData, 0, userListByteData.Length);
                     return;
                 }
 
+                //4.채팅 시작하기->기존 채팅방 유무 확인
+                if (parsedMessage.Contains("<ChattingStart>"))
+                {//기존 채팅방이 있는지->sender, receiver userID로 query 검색
+                    string isRoom = IsRoom(senderNickName, receiverNickName);
+                    if (isRoom == "")//방을 nickname 통해서 검색해야하나?
+                    {
+                        //채팅방 생성
+                        dbmanager.RunQuery("insert into Room(User1,User2) VALUES("+receiverNickName+","+senderNickName+")");//roomtable에 insert
+                       
+                    }
+                    else
+                    {//기존에 채팅방 있음.
+                        
+                    }
+                    parsedMessage = string.Format("{0}%{1}<{2}#ChattingStart>", receiverID, receiverNickName, isRoom);//실시간 채팅 시작 부분
+                    sendByteData = Encoding.Default.GetBytes(parsedMessage);
+                    ClientManager.clientDic[senderNumber].tcpClient.GetStream().Write(sendByteData, 0, sendByteData.Length);
 
-
-                if (parsedMessage.Contains(""))
-
+                    parsedMessage = string.Format("{0}%{1}<{2}#ChattingStart>", senderID, senderNickName, isRoom);
+                    sendByteData = Encoding.Default.GetBytes(parsedMessage);
                     ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(sendByteData, 0, sendByteData.Length);
+                    return;
+                }
+                
+
+                //5.메세지 보내기
+                if (parsedMessage.Contains(""))
+                {
+                    roomID = splitedMsg[1].Split('#')[0];
+                    splitedMsg[1] = splitedMsg[1].Split('#')[1];
+                    ClientManager.clientDic[receiverNumber].tcpClient.GetStream().Write(sendByteData, 0, sendByteData.Length);//채팅 때마다 테이블에 추가. 아직 room 없음
+                    //실시간 채팅 상태이므로 읽음 상태 y
+                    dbmanager.RunQuery("insert into Chatting VALUES("+ int.Parse(roomID) + ",\"" + receiverID + "\",\"" + senderID + "\",\"" + splitedMsg[1] + "\",\"y\",\""+ DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\")");
+                }
             }
         }
 
-        private int GetClinetNumber(string targetClientName)
+        private int GetClinetNumber(string targetClientName)//올바른 ClientData를 통해 ClientNumber를 찾는 과정
         {
             foreach (var item in ClientManager.clientDic)
             {
-                if (item.Value.clientName == targetClientName)
+                if (item.Value.clientID == targetClientName)
                 {
                     return item.Value.clientNumber;
                 }
             }
             return -1;
+        }
+        private string IsRoom(string senderNickName,string receiverNickName)//채팅방 있는지 확인해주고 있으면 룸아이디 반환
+        {
+            string isRoom = dbmanager.RunQuery("SELECT distinct idRoom\r\nFROM Room\r\nWHERE " +
+                        "(Room.USER1 = \"" + receiverNickName + "\" AND Room.USER2=\"" + senderNickName + "\") OR" +
+                        " (Room.USER1 =\"" + senderNickName + "\" AND Room.USER2 = \"" + receiverNickName + "\");\r\n");
+            return isRoom;
+        }
+        private void RoomManager(string senderNickName,string receiverNickName)
+        {
+
         }
 
         private void MainServerStart()
