75ef10487a426f83aa7c4d429fc01e59b116fc0c
yjh47 <w59147@gmail.com>
2022-12-05 20:53:20 +0900

    Merge Master

--- DBP 관리/Form_ChangeProfile.cs
+++ DBP 관리/Form_ChangeProfile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,13 +17,67 @@ namespace DBP_관리
     {
         public DataEventHandler dataEvent;
         string receivedData;
-        public Form_ChangeProfile(string data)
+        public Form_ChangeProfile(string data, string ID)
         {
             receivedData = data;
             InitializeComponent();
+            Load_User_Config(ID);
         }
+        
+		private void Load_User_Config(string user_ID) {
+            //원래 저장된 테마 데이터 불러오기 및 적용
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+            string selectedColor = "";
+
+			string Connection_string = "Server=115.85.181.212;Port=3306;Database=s5469698;Uid=s5469698;Pwd=s5469698;CharSet=utf8;";
+			string query = "SELECT Back_Color FROM USER_Config WHERE User_ID = " + user_ID + ";";
+
+			using (MySqlConnection connection = new MySqlConnection(Connection_string)) {
+				connection.Open();
+				MySqlCommand cmd = new MySqlCommand(query, connection);
+				MySqlDataReader rdr = cmd.ExecuteReader();
+
+				if (!rdr.Read()) {
+					//기존 데이터가 없다면 기본 다크 모드
+					selectedColor = "DarkMode";
+					Apply_Mode(selectedColor);
+					return;
+				}
+
+				selectedColor = rdr[0].ToString();
+			}
+			Apply_Mode(selectedColor);
+		}
+
+		private void Apply_Mode(string selectedColor) {
+			if (selectedColor.Equals("DarkMode")) {
+				this.BackColor = Color.FromArgb(66, 66, 66);
+				this.ForeColor = Color.White;
+			}
+			else {
+				this.BackColor = Color.White;
+				this.ForeColor = Color.FromArgb(66, 66, 66);
+			}
+            this.pictureBox1.BackColor = this.BackColor;
+            this.label1.BackColor = this.BackColor;
+			this.label1.ForeColor = this.ForeColor;
+			this.label2.BackColor = this.BackColor;
+			this.label2.ForeColor = this.ForeColor;
+			this.label3.BackColor = this.BackColor;
+			this.label3.ForeColor = this.ForeColor;
+			this.label4.BackColor = this.BackColor;
+			this.label4.ForeColor = this.ForeColor;
+            this.label7.BackColor = this.BackColor;
+            this.label7.ForeColor = this.ForeColor;
+            this.btn_OK.BackColor = this.ForeColor;
+			this.btn_OK.ForeColor = this.BackColor;
+			this.button1.BackColor = this.ForeColor;
+			this.button1.ForeColor = this.BackColor;
+			this.button2.BackColor = this.ForeColor;
+			this.button2.ForeColor = this.BackColor;
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
--- DBP 관리/Form_Font.cs
+++ DBP 관리/Form_Font.cs
@@ -18,9 +18,56 @@ namespace DBP_관리
         {
             InitializeComponent();
             this.userid = Convert.ToInt32(user_id);
+            Load_User_Config(user_id);
         }
 
-        private void button_BackColor_Click(object sender, EventArgs e)
+		private void Load_User_Config(string user_ID) {
+			//원래 저장된 테마 데이터 불러오기 및 적용
+
+			string selectedColor = "";
+
+			string Connection_string = "Server=115.85.181.212;Port=3306;Database=s5469698;Uid=s5469698;Pwd=s5469698;CharSet=utf8;";
+			string query = "SELECT Back_Color FROM USER_Config WHERE User_ID = " + user_ID + ";";
+
+			using (MySqlConnection connection = new MySqlConnection(Connection_string)) {
+				connection.Open();
+				MySqlCommand cmd = new MySqlCommand(query, connection);
+				MySqlDataReader rdr = cmd.ExecuteReader();
+
+				if (!rdr.Read()) {
+					//기존 데이터가 없다면 기본 다크 모드
+					selectedColor = "DarkMode";
+					Apply_Mode(selectedColor);
+					return;
+				}
+
+				selectedColor = rdr[0].ToString();
+			}
+			Apply_Mode(selectedColor);
+		}
+
+		private void Apply_Mode(string selectedColor) {
+			if (selectedColor.Equals("DarkMode")) {
+				this.BackColor = Color.FromArgb(66, 66, 66);
+				this.ForeColor = Color.White;
+			}
+			else {
+				this.BackColor = Color.White;
+				this.ForeColor = Color.FromArgb(66, 66, 66);
+			}
+            this.label_BackColor_Title.BackColor = this.BackColor;
+			this.label_BackColor_Title.ForeColor = this.ForeColor;
+			this.label3.BackColor = this.BackColor;
+			this.label3.ForeColor = this.ForeColor;
+            this.label4.BackColor = this.BackColor;
+			this.label4.ForeColor = this.ForeColor;
+            this.groupBox_BackColor.BackColor = this.BackColor;
+			this.groupBox_BackColor.ForeColor = this.ForeColor;
+            this.button_BackColor.BackColor = this.ForeColor;
+			this.button_BackColor.ForeColor = this.BackColor;
+		}
+
+		private void button_BackColor_Click(object sender, EventArgs e)
         {
             if(comboBox3.SelectedItem == null && comboBox4.SelectedItem == null)
             {
